ddccd87738c7d76eb3bcc8669687e7d575f33e47
Viktor Albert <albertviktor@utitech.ro>
2024-10-27 01:27:39 +0300

    Relay controller added

--- src/main/java/com/utitech/carwash/CarwashApplication.java
+++ src/main/java/com/utitech/carwash/CarwashApplication.java
@@ -1,12 +1,106 @@
 package com.utitech.carwash;
 
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.util.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarwashApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(CarwashApplication.class, args);
+
+        final var console = new Console();
+
+        // Print program title/header
+        console.title("<-- The Pi4J Project -->", "Minimal Example project");
+
+        // ************************************************************
+        //
+        // WELCOME TO Pi4J:
+        //
+        // Here we will use this getting started example to
+        // demonstrate the basic fundamentals of the Pi4J library.
+        //
+        // This example is to introduce you to the boilerplate
+        // logic and concepts required for all applications using
+        // the Pi4J library.  This example will do use some basic I/O.
+        // Check the pi4j-examples project to learn about all the I/O
+        // functions of Pi4J.
+        //
+        // ************************************************************
+
+        // ------------------------------------------------------------
+        // Initialize the Pi4J Runtime Context
+        // ------------------------------------------------------------
+        // Before you can use Pi4J you must initialize a new runtime
+        // context.
+        //
+        // The 'Pi4J' static class includes a few helper context
+        // creators for the most common use cases.  The 'newAutoContext()'
+        // method will automatically load all available Pi4J
+        // extensions found in the application's classpath which
+        // may include 'Platforms' and 'I/O Providers'
+        var pi4j = Pi4J.newAutoContext();
+
+        // ------------------------------------------------------------
+        // Output Pi4J Context information
+        // ------------------------------------------------------------
+        // The created Pi4J Context initializes platforms, providers
+        // and the I/O registry. To help you to better understand this
+        // approach, we print out the info of these. This can be removed
+        // from your own application.
+        // OPTIONAL
+
+        // Here we will create I/O interfaces for a (GPIO) digital output
+        // and input pin. Since no specific 'provider' is defined, Pi4J will
+        // use the default `DigitalOutputProvider` for the current default platform.
+        var led = pi4j.digitalOutput().create(17);
+
+        var buttonConfig = DigitalInput.newConfigBuilder(pi4j)
+                .id("button")
+                .name("Press button")
+                .address(12)
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(3000L);
+        var button = pi4j.create(buttonConfig);
+        button.addListener(e -> {
+            if (e.state() == DigitalState.LOW) {
+                led.high();
+            }
+        });
+
+        // OPTIONAL: print the registry
+
+        for (int i = 0; i < 10; i++) {
+
+        } {
+            if (led.equals(DigitalState.HIGH)) {
+                console.println("LED low");
+                led.low();
+            } else {
+                console.println("LED high");
+                led.high();
+            }
+            Thread.sleep(500 );
+        }
+
+        // ------------------------------------------------------------
+        // Terminate the Pi4J library
+        // ------------------------------------------------------------
+        // We we are all done and want to exit our application, we must
+        // call the 'shutdown()' function on the Pi4J static helper class.
+        // This will ensure that all I/O instances are properly shutdown,
+        // released by the the system and shutdown in the appropriate
+        // manner. Terminate will also ensure that any background
+        // threads/processes are cleanly shutdown and any used memory
+        // is returned to the system.
+
+        // Shutdown Pi4J
+        pi4j.shutdown();
     }
 }
