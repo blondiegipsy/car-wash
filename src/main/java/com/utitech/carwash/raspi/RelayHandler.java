8477bbffd8dcec845ed26ea666b16f9e8770b79b
Albert Viktor <viktoralbert777@gmail.com>
2024-10-26 19:40:12 +0300

    Relay Configuration changed

--- src/main/java/com/utitech/carwash/raspi/RelayHandler.java
+++ src/main/java/com/utitech/carwash/raspi/RelayHandler.java
@@ -1,6 +1,7 @@
 package com.utitech.carwash.raspi;
 
 import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.platform.Platforms;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,16 @@ public class RelayHandler {
 
         Platforms platforms = pi4j.platforms();
 
+        pi4j.describe().size();
+
         platforms.describe().print(System.out);
+
+        final int PIN_LED = 17; // PIN 15 = BCM 22
+
+        var led = pi4j.digitalOutput().create(PIN_LED);
+
+        while (true) {
+            led.high();
+        }
     }
 }
